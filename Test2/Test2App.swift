16bbf73038b07e8be63a17e91779d70eb333c4cc
forrobdev <robin.despaquis@gmail.com>
2026-01-29 09:40:18 +0100

    Initial Commit

---
+++ Test2/Test2App.swift
@@ -0,0 +1,17 @@
+//
+//  Test2App.swift
+//  Test2
+//
+//  Created by Robin Despaquis on 29/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
